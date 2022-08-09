51089e845b0b61671fc39ff2c91e140f667e4490
garu2 <elgafo.nakama@gmail.com>
2022-08-09 12:11:31 -0400

    ADD: styling header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,25 @@
 import Link from "next/link";
-import styles from "../styles/Header.module.css"
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <div className="contenedor">
-                <div>
-
-                </div>
+            <div className={`container  ${styles.menuLogo}`}>
+                <Link href="/">
+                    <a>
+                        <Image src="/logo.svg" width={170} height={60} alt="Image Logo" />
+                    </a>
+                </Link>
                 <nav>
                     <Link href="/">Home</Link>
                     <Link href="/about">About</Link>
                     <Link href="/blog">Blog</Link>
                     <Link href="/contact">Contact</Link>
                 </nav>
+            </div>
+            <div className={`container ${styles.bigTitle}`}>
+                <h2>Blog Page</h2>
             </div>
         </header>
     )
